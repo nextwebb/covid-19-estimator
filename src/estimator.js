c52485b4e3d6efce7df56df6abf20ba3253c6815
nextwebb <oaikhenahpeterson@gmail.com>
2020-04-11 14:53:14 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,37 @@
-const covid19ImpactEstimator = (data) => data;
+import data from './data';
 
+// const data = require('./data');
+
+// eslint-disable-next-line no-shadow
+const covid19ImpactEstimator = (data) => {
+  // impact && severe Impact
+  const impact = {};
+  const severeImpact = {};
+  const { reportedCases } = data;
+  const { timeToElapse } = data;
+  const roundedDown = (timeToElapse / 3);
+  const power = Math.floor(roundedDown);
+  impact.currentlyInfected = (reportedCases * 10);
+  // eslint-disable-next-line no-restricted-properties
+  impact.infectionsByRequestedTime = (impact.currentlyInfected * Math.pow(2, power));
+
+  severeImpact.currentlyInfected = (reportedCases * 50);
+  // eslint-disable-next-line no-restricted-properties
+  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * Math.pow(2, power));
+
+
+  const input = data;
+  return {
+    data: input,
+    impact,
+    severeImpact
+  };
+};
+
+// const output = covid19ImpactEstimator(data);
+// eslint-disable-next-line no-console
+// console.log(output);
+
+
+// module.exports = covid19ImpactEstimator;
 export default covid19ImpactEstimator;
